717e5fa20837b9a44c30e24db342b72837ce3419
Juan Felipe Rojas Muñoz <juanferojas19@gmail.com>
2023-07-17 13:35:13 -0500

    interfaz programa 1

--- virtualfilm.py
+++ virtualfilm.py
@@ -1,69 +1,127 @@
+import tkinter as tk
+
 #Juan Felipe Rojas 
-#Proyecto cine primera parte 
+# Segunda parte 
 
+def verificar_contrasena():
+    global cliente, password
+    password_input = password_entry.get()
+    if password_input != 'crack':
+        result_label.config(text='Contraseña incorrecta')
+    else:
+        result_label.config(text='Contraseña correcta')
+        elegir_pelicula()
 
+def elegir_pelicula():
+    login_frame.pack_forget()
+    pelicula_frame.pack()
 
-print("\t!Bienvenido a cine colombia ")
-cliente=str(input('digite usuario => '))
-password=str(input('digite contraseña => '))
+def calcular_precio():
+    global price
+    opcion_pelicula = opcion_var.get()
+    if opcion_pelicula == 1:
+        price = 100
+    elif opcion_pelicula == 2:
+        price = 200
+    else:
+        price_label.config(text='Pelicula no disponible')
+        return
 
+    elegir_teatro()
 
-while password != 'crack':
-  print('contraseña icorrecta ')
-  password=str(input('digite contraseña => '))
+def elegir_teatro():
+    pelicula_frame.pack_forget()
+    teatro_frame.pack()
 
-print("contraseña correcta ")
+def calcular_precio_final():
+    global price
+    opcion_teatro = teatro_var.get()
+    if opcion_teatro == 1:
+        price *= 1.0
+    elif opcion_teatro == 2:
+        price *= 0.9
+    elif opcion_teatro == 3:
+        price *= 0.8
+    elif opcion_teatro == 4:
+        price *= 0.6
+    elif opcion_teatro == 5:
+        price *= 0.4
+    else:
+        price_label.config(text='Opcion inválida')
+        return
 
+    desea_comida()
 
-print("\telegir pelicula")
-print("\n1. Avatar ")
-print("2. Avengers")
-opcion=str(input('digite opcion => '))
+def desea_comida():
+    teatro_frame.pack_forget()
+    comida_frame.pack()
 
-if opcion=='1':
-  price=100
-  print(price)
-elif opcion=='2':
-  price=200
-  print(price)
-else:
-  print('pelicula no disponible')
-  exit()
-  
-print("\t elegir teatro ")
-print("1. chile ")
-print("2. gran estacion ")
-print("3. hayuelos ")
-print("4. titan")
-print("5. colina ")
-option=str(input('digite opcion => '))
+def calcular_precio_final_con_comida():
+    global price
+    opcion_comida = comida_var.get()
+    if opcion_comida == 1:
+        price += 20
 
-if option =='1':
-  price=(price*100.0)/100
-  print('$', price)
-elif option =='2':
-  price=(price*90.0)/100
-  print('$', price)
-elif option =='3':
-  price=(price*80.0)/100
-  print('$', price)
-elif option =='4':
-  price=(price*60.0)/100
-  print('$', price)
-elif option =='5':
-  price=(price*40.0)/100
-  print('$', price)
-else:
-  print('opcion invalida')
-  exit()
+    price_label.config(text='Precio: $' + str(price))
 
-print('desea comida ')
-print("si o no")
-opciones=str(input('digite => '))
+# Crear la ventana
+ventana = tk.Tk()
+ventana.title('VirtualFilm')
+ventana.geometry('400x300')
 
-if opciones =='si':
-  price+=20
-  print(price)
-else:
-  print(price)
-  exit()
+# Variables globales
+price = 0
+
+# Frame para el login
+login_frame = tk.Frame(ventana)
+login_frame.pack(pady=20)
+tk.Label(login_frame, text='!Bienvenido a VirtualFilm!', font=('Arial', 16)).pack()
+tk.Label(login_frame, text='Digite usuario => ').pack()
+cliente = tk.StringVar()
+tk.Entry(login_frame, textvariable=cliente).pack()
+tk.Label(login_frame, text='Digite contraseña => ').pack()
+password = tk.StringVar()
+password_entry = tk.Entry(login_frame, textvariable=password, show='*')
+password_entry.pack()
+tk.Button(login_frame, text='Ingresar', command=verificar_contrasena).pack()
+result_label = tk.Label(login_frame, text='')
+result_label.pack()
+
+# Frame para elegir la película
+pelicula_frame = tk.Frame(ventana)
+tk.Label(pelicula_frame, text='Elegir película', font=('Arial', 14)).pack()
+opcion_var = tk.IntVar()
+opcion_var.set(1)
+tk.Radiobutton(pelicula_frame, text='Avatar', variable=opcion_var, value=1).pack()
+tk.Radiobutton(pelicula_frame, text='Avengers', variable=opcion_var, value=2).pack()
+tk.Button(pelicula_frame, text='Continuar', command=calcular_precio).pack()
+pelicula_frame.pack_forget()
+
+# Frame para elegir el teatro
+teatro_frame = tk.Frame(ventana)
+tk.Label(teatro_frame, text='Elegir teatro', font=('Arial', 14)).pack()
+teatro_var = tk.IntVar()
+teatro_var.set(1)
+tk.Radiobutton(teatro_frame, text='Chile', variable=teatro_var, value=1).pack()
+tk.Radiobutton(teatro_frame, text='Gran Estacion', variable=teatro_var, value=2).pack()
+tk.Radiobutton(teatro_frame, text='Hayuelos', variable=teatro_var, value=3).pack()
+tk.Radiobutton(teatro_frame, text='Titan', variable=teatro_var, value=4).pack()
+tk.Radiobutton(teatro_frame, text='Colina', variable=teatro_var, value=5).pack()
+tk.Button(teatro_frame, text='Continuar', command=calcular_precio_final).pack()
+teatro_frame.pack_forget()
+
+# Frame para elegir si desea comida
+comida_frame = tk.Frame(ventana)
+tk.Label(comida_frame, text='¿Desea comida?', font=('Arial', 14)).pack()
+comida_var = tk.IntVar()
+comida_var.set(1)
+tk.Radiobutton(comida_frame, text='Sí', variable=comida_var, value=1).pack()
+tk.Radiobutton(comida_frame, text='No', variable=comida_var, value=2).pack()
+tk.Button(comida_frame, text='Continuar', command=calcular_precio_final_con_comida).pack()
+comida_frame.pack_forget()
+
+# Frame para mostrar el precio final
+price_label = tk.Label(ventana, text='Precio:')
+price_label.pack()
+
+ventana.mainloop()
